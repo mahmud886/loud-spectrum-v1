8226a3470684912a293b188c1e8045ffa9fc69c1
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2025-04-13 00:36:34 +0600

    two premium local fonts updated

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -12,20 +12,20 @@ const Hero = () => {
       {/* Content */}
       <div className="relative z-10 flex h-full w-3/6 flex-col items-center justify-center gap-[40px] px-[80px]">
         <div>
-          <h1 className="font-sans text-[60px] leading-[120%] tracking-normal text-white pb-5">
+          <h1 className="font-sans text-[60px] font-normal leading-[120%] tracking-normal text-white pb-5">
             {t('The_Premier_Terpene_Experience')}
           </h1>
-          <p className="font-mono text-[20px] leading-[120%] text-white">
+          <p className="font-mono text-[20px] font-normal leading-[120%] text-white">
             {t(
               'Enhance_your_products_with_the_widest_range_of_premium_terpenes_scientifically_crafted_to_elevate_every_experience',
             )}{' '}
           </p>
         </div>
-        <div className="inline-flex gap-[15px] self-start">
-          <button className="special-button h-[42px] w-[177px] cursor-grab rounded-full">
+        <div className="w-full inline-flex gap-[15px] self-start">
+          <button className="special-button py-2 px-6 cursor-grab rounded-full">
             {t('Explore_the_shop')}
           </button>
-          <button className="outline-button-black h-[42px] w-[177px] cursor-grab rounded-full">
+          <button className="outline-button-black py-2 px-6 cursor-grab rounded-full">
             {t('Try_a_Sample_Pack')}
           </button>
         </div>
