10eb632c4f32ff55da49970214e34e9077cd396c
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2025-04-17 21:33:29 +0600

    Terpene Shop component i18n done

--- components/containers/shop/TerpeneProductsContainer.jsx
+++ components/containers/shop/TerpeneProductsContainer.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import ProductCard from '@/components/product/ProductCard';
 import ProductPagination from '@/components/containers/shop/ProductPagination';
 import RadioGroupButtons from '@/components/ui/RadioGroupButtons';
+import { useTranslations } from 'next-intl';
 
 const TerpeneProductsContainer = () => {
+  const t = useTranslations('TerpeneShop');
   return (
     <div className="container pt-[100px]">
       <div className="flex items-start justify-between gap-12">
         <div className="min-w-[20%]">
           <div className="mb-12">
             <h5 className="text-umbra-100 font-sans text-[22px] leading-[130%] font-normal tracking-normal">
-              All Products <span className="text-umbra-40">(62)</span>
+              {t('AllProducts')} <span className="text-umbra-40">(62)</span>
             </h5>
           </div>
           <RadioGroupButtons />
--- components/product/ProductCard.jsx
+++ components/product/ProductCard.jsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
+import { useTranslations } from 'next-intl';
 
 const ProductCard = () => {
+  const t = useTranslations('TerpeneShop.ProductCard');
   return (
     <div className="group cursor-pointer">
       <div className="group-hover:border-umbra-100 h-[384px] w-[305px] border border-transparent bg-[#F0F0F0] p-5 transition-colors duration-200 ease-in-out 2xl:h-[510px] 2xl:w-[382px]">
         <div className="flex justify-end">
-          <button className="main-button-black h-[42px] w-[114px] rounded-full opacity-0 transition-opacity duration-200 ease-in-out group-hover:opacity-100">
-            Buy Now
+          <button className="main-button-black rounded-full px-6 py-3 opacity-0 transition-opacity duration-200 ease-in-out group-hover:opacity-100">
+            {t('buyNow')}
           </button>
         </div>
         <div className="flex items-center justify-center overflow-hidden">
@@ -19,9 +21,7 @@ const ProductCard = () => {
             priority={true}
           />
         </div>
-        <button className="border-umbra-100 h-[22px] w-[114px] rounded-[3px] border text-[12px] font-normal">
-          Cannabis Derived
-        </button>
+        <button className="border-umbra-100 rounded-[3px] border px-2 text-[12px] font-normal">{t('tag')}</button>
       </div>
       <div className="mt-[15px]">
         <h2 className="text-[22px] font-normal">Mango OG</h2>
