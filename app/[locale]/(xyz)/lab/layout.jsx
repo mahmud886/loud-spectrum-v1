726128345e0cf9915ffe208e0ee32ca64cdba792
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2025-04-20 00:57:08 +0600

    the lab section completed with i18n

--- app/[locale]/(xyz)/lab/layout.jsx
+++ app/[locale]/(xyz)/lab/layout.jsx
@@ -7,15 +7,6 @@ const TheLabLayout = ({ children }) => {
     <div>
       <TheLabHero />
       {children}
-      <div className="mx-auto w-[1440px]">
-        <Image
-          src="/assets/images/the-lab-section-mask.svg"
-          alt="section-mask"
-          width={1440}
-          height={195}
-          className="h-full w-full"
-        />
-      </div>
     </div>
   );
 };
