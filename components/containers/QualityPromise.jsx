0e6abbbb8fff3308901f137d8afb5ff41e01f455
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2025-04-20 22:59:46 +0600

    quality promise fix

--- components/containers/QualityPromise.jsx
+++ components/containers/QualityPromise.jsx
@@ -9,6 +9,15 @@ const QualityPromise = () => {
       <div className="py-[80px]">
         <div className="relative z-0 flex min-h-[796px] items-center justify-center p-[80px]">
           <div className="absolute inset-0 -z-10">
+            <div className="absolute top-[-2px] left-0 z-20">
+              <Image
+                src="/assets/images/home-quality-top-mask.png"
+                alt="Wave"
+                width={1280}
+                height={84}
+                className="h-[84px] w-[1280px] object-cover"
+              />
+            </div>
             <Image
               src="/assets/images/quality-promise/grid-texture.png"
               alt="Background"
@@ -16,6 +25,15 @@ const QualityPromise = () => {
               height={796}
               className="h-full w-full object-cover"
               priority
+            />
+          </div>
+          <div className="absolute bottom-[-2px] left-0 z-20">
+            <Image
+              src="/assets/images/home-quality-bottom-mask.png"
+              alt="Wave"
+              width={1280}
+              height={84}
+              className="h-[84px] w-[1280px] object-cover"
             />
           </div>
           <div className="flex items-center justify-between gap-[126px]">
