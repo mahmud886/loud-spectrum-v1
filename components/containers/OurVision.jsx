9161767997705b305cba0bd0c2894fa9c9155d31
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2025-04-26 15:21:06 +0600

    responsive about page

--- components/containers/OurVision.jsx
+++ components/containers/OurVision.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import Image from 'next/image';
 import { useTranslations } from 'next-intl';
+import Image from 'next/image';
 
 const OurVision = () => {
   const t = useTranslations('OurVision');
   return (
     <div className="container">
-      <div className="relative h-[592px] w-full overflow-hidden">
-        <Image src="/assets/images/our-vision.png" alt="Background" fill className="object-cover" priority />
-        <div className="relative z-20 flex h-full items-center px-[100px]">
-          <div className="max-w-[400px] space-y-6 text-white">
+      <div className="relative h-[720px] w-full overflow-hidden md:h-[592px]">
+        <div className="absolute inset-0 z-10 mx-auto h-full w-full">
+          <Image src="/assets/images/about-us/our-vision.png" alt="Background" fill className="object-cover" priority />
+        </div>
+        <div className="relative z-20 flex h-full items-end px-5 pt-10 md:items-center md:px-[100px]">
+          <div className="space-y-6 pb-10 text-white md:max-w-[400px] md:pb-0">
             <h6 className="heading-6">{t('subtitle')}</h6>
-            <h2 className="font-sans text-[44px] leading-[120%] font-normal tracking-normal">{t('title')}</h2>
+            <h2 className="font-sans text-[32px] leading-[120%] font-normal tracking-normal md:text-[44px]">
+              {t('title')}
+            </h2>
             <p className="text-white-100 font-mono text-[16px] leading-[140%] font-normal">{t('description')}</p>
           </div>
         </div>
