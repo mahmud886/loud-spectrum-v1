6cec4abedcab53566315cbe93679228086b38cf1
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2025-04-26 17:24:45 +0600

    fixed slider on new page

--- components/ui/carousel.jsx
+++ components/ui/carousel.jsx
@@ -117,7 +117,11 @@ function CarouselItem({ className, ...props }) {
       role="group"
       aria-roledescription="slide"
       data-slot="carousel-item"
-      className={cn('min-w-0 shrink-0 grow-0 basis-full', orientation === 'horizontal' ? 'pl-4' : 'pt-4', className)}
+      className={cn(
+        'min-w-0 shrink-0 grow-0 basis-[90%] md:basis-full',
+        orientation === 'horizontal' ? 'pl-4' : 'pt-4',
+        className,
+      )}
       {...props}
     />
   );
