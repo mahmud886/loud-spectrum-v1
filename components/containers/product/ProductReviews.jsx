fe4f069a2ddb1e9b2269347450529352eb5d2b91
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2025-04-27 18:46:50 +0600

    product page and try-sampe-pack design done

--- components/containers/product/ProductReviews.jsx
+++ components/containers/product/ProductReviews.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import ReviewAndTestimonialCarouselWithProgress from '@/components/carousels/ReviewAndTestimonialCarouselWithProgress';
+import ProductReviewCard from '@/components/product/ProductReviewCard';
 
 const ProductReviews = () => {
   return (
-    <div className={'container py-[80px]'}>
-      <h2 className="text-umbra-100 font-sans text-[50px]">Product Reviews</h2>
+    <div className={'container md:pb-[80px]'}>
+      <h2 className="text-umbra-100 font-sans text-[32px] leading-[120%] font-normal md:text-[44px]">
+        Reviews <span className="text-umbra-40">(6)</span>
+      </h2>
+      <div>
+        <ReviewAndTestimonialCarouselWithProgress>
+          <ProductReviewCard />
+        </ReviewAndTestimonialCarouselWithProgress>
+      </div>
     </div>
   );
 };
