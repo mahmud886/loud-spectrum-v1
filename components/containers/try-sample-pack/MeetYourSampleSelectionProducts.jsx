acf5a371c5f3d3469ae18c4ad446f5d05261bb85
mahmud886 <iqbal886mahmud@gmail.com>
2025-07-18 01:18:34 +0600

    Refactor sample pack and new page to use dynamic product data
    Updated the try-sample-pack and new page layouts to fetch and display products dynamically from the API. Refactored MeetYourSampleSelectionProducts, NewPageHero, and SamplePackCard components to accept and render product data, replacing hardcoded values and static imports. Improved carousel rendering to map over product lists and pass product details to child components.

--- components/containers/try-sample-pack/MeetYourSampleSelectionProducts.jsx
+++ components/containers/try-sample-pack/MeetYourSampleSelectionProducts.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
-import SamplePackCard from '@/components/product/SamplePackCard';
 import ProductCarouselWithProgress from '@/components/carousels/ProductCarouselWithProgress';
+import SamplePackCard from '@/components/product/SamplePackCard';
+import { CarouselItem } from '@/components/ui/carousel';
+import { getCategoryProducts } from '@/services/get-category-products';
 
-const MeetYourSampleSelectionProducts = () => {
+const MeetYourSampleSelectionProducts = async ({ samplePackCategory }) => {
+  const listOfProducts = await getCategoryProducts();
+
+  const filteredSamplePackProducts =
+    (await listOfProducts?.data?.filter((product) =>
+      samplePackCategory?.some((category) => product.category_id === category._id),
+    )) || [];
+
+  console.log('filteredSamplePackProducts', filteredSamplePackProducts);
   return (
     <div className="container py-[60px] pt-[60px] md:py-[160px] md:pt-[160]">
       <div className="space-y-4 md:space-y-6">
@@ -15,7 +24,14 @@ const MeetYourSampleSelectionProducts = () => {
       </div>
       <div className="mt-3 md:mt-6">
         <ProductCarouselWithProgress>
-          <SamplePackCard />
+          {filteredSamplePackProducts?.slice(0, 20).map((product) => (
+            <CarouselItem
+              key={product._id}
+              className="pl-2 sm:basis-1 md:basis-1/2 md:pl-2 lg:basis-1/4 xl:basis-1/4 2xl:basis-1/4"
+            >
+              <SamplePackCard product={product} />
+            </CarouselItem>
+          ))}
         </ProductCarouselWithProgress>
       </div>
     </div>
